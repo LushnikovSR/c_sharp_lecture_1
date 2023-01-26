a308bc1d2673b834c6fbf9dc154ceac06eea32cc
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-26 18:08:09 +0300

    add Home Work Friends and Dog

---
+++ HomeWork_FriendsAndDog/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Задача\nДва друга двигаются на встречу друг другу по прямой, при этом у одного из них есть собака"+
+" которая бежит от одно друга к другому и возвращается двигаясь быстрее чем кто либо из друзей. Вопрос сколько раз собака добежит"+
+" до каждого друга?\nДля решения задачи введите следующие параметры:");
+int friends = 2;
+Console.WriteLine("Скорость первого друга (км/ч): ");
+double firstFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Скорость второго друга (км/ч): ");
+double secondFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Начальное расстояние между друзьями(м): ");
+double distance = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Скорость собаки (км/ч): ");
+double dogSpeed = Convert.ToDouble(Console.ReadLine());
+int count = 0;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friends == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friends = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friends = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine("Хотите узнать ответ? (Введите 'да' или 'нет'): ");
+string answer = Console.ReadLine();
+if (answer.ToLower() == "да")
+{
+    Console.WriteLine($"Собака прибежит {count} раз");
+}
+else{
+    if (answer.ToLower() == "нет")
+{
+    Console.WriteLine("Удачи с решением задачи!");
+}
+else{
+    Console.WriteLine("Веденная команда не соответствует предложенным вариантам. Удачи с решением задачи.");
+}
+}
