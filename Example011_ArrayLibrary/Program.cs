34bc8e258b01ab32b239aecf0e5d10da968b46c5
Stanislav_Lushnikov <lust48h@gmail.com>
2023-01-28 22:31:59 +0300

    add Example 011 Array Library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿// Лекция 2
+// void это имя типа значения возвращаемого функцией которое указывает что функции ничего не возвращает
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // -1 указана как индикатор ситуации при которой заданное число небыло найдено в массиве
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //по умолчанию массив из 10 элементов заполнен нулями, для его заполнения создадим функцию
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
